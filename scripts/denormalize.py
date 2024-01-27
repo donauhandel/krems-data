10de2eb93e12a6e417095dda12258c5233f0b071
csae8092 <peter.andorfer@oeaw.ac.at>
2024-01-27 11:20:42 +0100

    closes #2

--- scripts/denormalize.py
+++ scripts/denormalize.py
@@ -1,5 +1,8 @@
+from collections import defaultdict
+
 import lxml.etree as ET
 from acdh_tei_pyutils.tei import TeiReader
+from acdh_tei_pyutils.utils import make_entity_label
 from config import LISTORG
 from config import LISTPERSON
 from config import LISTPLACE
@@ -8,6 +11,36 @@ from tqdm import tqdm
 person_xml = TeiReader(LISTPERSON)
 org_xml = TeiReader(LISTORG)
 nsmap = person_xml.nsmap
+
+for bad in org_xml.any_xpath(".//tei:listPerson"):
+    bad.getparent().remove(bad)
+
+print(f"writing persons into {LISTORG}")
+lookup = defaultdict(set)
+for x in person_xml.any_xpath(".//tei:person[./tei:affiliation]"):
+    xml_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
+    label, _ = make_entity_label(x)
+    for y in x.xpath(".//tei:orgName[@key]", namespaces=nsmap):
+        key = y.attrib["key"][1:]
+        lookup[key].add(f"{xml_id}|{label}")
+
+for x in org_xml.any_xpath(".//tei:org"):
+    xml_id = x.attrib["{http://www.w3.org/XML/1998/namespace}id"]
+    persons = lookup[xml_id]
+    if persons:
+        listperson = ET.Element("{http://www.tei-c.org/ns/1.0}listPerson")
+        listperson.attrib["type"] = "affiliated"
+        x.append(listperson)
+        for y in persons:
+            key, label = y.split("|")
+            person = ET.Element("{http://www.tei-c.org/ns/1.0}person")
+            persname = ET.Element("{http://www.tei-c.org/ns/1.0}persName")
+            person.append(persname)
+            persname.attrib["key"] = f"#{key}"
+            persname.text = label
+            listperson.append(person)
+org_xml.tree_to_file(LISTORG)
+
 
 print("make org-dict")
 orgs = {}
