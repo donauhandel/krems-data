7a1464a2c683e065eaf07538b0c659c0a404cb3a
csae8092 <p.andorfer@gmail.com>
2025-01-08 12:29:22 +0100

    wow, this was a hard one to find out that I need to add a deepcopy of the lxml node

--- scripts/add_coords_to_places_in_listorg.py
+++ scripts/add_coords_to_places_in_listorg.py
@@ -2,14 +2,13 @@ from config import LISTPLACE, LISTORG
 from acdh_tei_pyutils.tei import TeiReader
 from acdh_tei_pyutils.utils import get_xmlid
 from acdh_xml_pyutils.xml import NSMAP
+import copy
 
 
 doc = TeiReader(LISTPLACE)
-
 lookup_dict = {}
 for x in doc.any_xpath(".//tei:place[@xml:id]"):
     xmlid = get_xmlid(x)
-    print(xmlid)
     try:
         geo = x.xpath(".//tei:geo", namespaces=NSMAP)[0]
     except IndexError:
@@ -18,14 +17,20 @@ for x in doc.any_xpath(".//tei:place[@xml:id]"):
 
 
 doc = TeiReader(LISTORG)
+no_match = set()
+for bad in doc.any_xpath(".//tei:geo"):
+    bad.getparent().remove(bad)
+doc.tree_to_file(LISTORG)
+
+doc = TeiReader(LISTORG)
 for x in doc.any_xpath(".//tei:location"):
-    try:
-        key = x.xpath("./tei:placeName/@key", namespaces=NSMAP)[0]
-    except IndexError:
-        continue
+    key = x.xpath("./tei:placeName/@key", namespaces=NSMAP)[0]
     try:
         geo = lookup_dict[key]
+        copy_geo = copy.deepcopy(geo)
+        x.insert(1, copy_geo)
     except KeyError:
-        continue
-    x.append(geo)
+        print(f"no match for {key}")
 doc.tree_to_file(LISTORG)
+
+print(no_match)
